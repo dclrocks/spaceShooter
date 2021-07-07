ef531f58d664704851a8fc1ddeaf5cf3f5bb8918
Dave <david@davidclittle.com>
2021-07-07 11:23:56 -0700

    Updates to SpawnManager.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,18 +11,23 @@ public class Player : MonoBehaviour
     [SerializeField] private float _fireRate = 0.1f;
     [SerializeField] private float _canFire = -0.5f;
     [SerializeField] private int _lives = 3;
+    [SerializeField] private SpawnManager _spawnManager;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager GameObject.Find("SpawnManager").getComponent<SpawnManager>;
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The SpawnManager is NULL.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculatePlayerMovement();
-
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
             FireLaser();
@@ -61,6 +66,7 @@ public class Player : MonoBehaviour
 
         if (_lives < 1)
         {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _enemyContainer;
+    [SerializeField] private bool _stopSpawning = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +26,16 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnRoutine()
     {
-        while (true)
+        while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8.0f, 8.0f), 7, 0);
-            Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(4.0f);
         }
+    }
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
     }
 }
